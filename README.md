26b1e93ac68d2b2cabad7ffe84fd66e65d79a208
Irresistible-Ishan <1ishanmanisingh@gmail.com>
2026-05-15 18:52:03 +0530

    fresh

---
+++ README.md
@@ -0,0 +1,13 @@
+# TimeCapsule-DatabaseAPP-JAVA-SWING-UI
+This is a notes app ;P or maybe not ? it has great potential , im making this for personal use as an open source tool , if its good u can use it too. Not your avg electron or webapp ;)
+
+# features :
+* **Burn**: Permanently destroys the file from the local SSD or HDD by overwriting the storage sectors with randomized binary data, making recovery virtually impossible, even with advanced forensic tools.
+
+* **Eliminate**: Completely removes the file from all connected sources, including linked cloud storage platforms and synchronized backups.
+
+* **Save**: Stores the file locally in a predefined directory or user-configured storage location.
+
+* **Export**: Transfers or synchronizes files to connected cloud storage services or linked external platforms.
+
+* **Import**: Supports importing data from virtually any existing file format. This includes text documents, PDFs, images, and other media types. For image-based content, the system automatically performs OCR (Optical Character Recognition) and directly extracts and integrates the detected text.
---
+++ main.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+class createFrame{
+    public JFrame createFrameFunction(String name , int sizex ,int sizey , int posx, int posy){
+        JFrame frame = new JFrame();
+        frame.setTitle(name);
+        frame.setSize(sizex,sizey);
+        frame.setLocation(posx,posy);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        return frame;
+    }
+    public JPanel createPanelFunction(JFrame frame,int x , int y , int sizex , int sizey){
+        JPanel panel = new JPanel();
+        panel.setBounds(x,y,sizex, sizey);
+        panel.setLayout(null);
+        panel.setVisible(true);
+        frame.add(panel);
+        return panel;
+    }
+}
+class Btn{
+    public JButton addbtn(Container frameOrPanel , String text , String hoverText , int x , int y , int sizex , int sizey){
+        JButton jbtn = new JButton();
+        ImageIcon btnskin = new ImageIcon("./assets/buttons/button-main.png");
+        jbtn.setText(text);
+        jbtn.setIcon(btnskin);
+        jbtn.setHorizontalTextPosition(JButton.CENTER);
+        jbtn.setVerticalTextPosition(JButton.CENTER);
+        jbtn.setBounds(x,y,sizex,sizey);
+        jbtn.setFocusable(true);
+        jbtn.setEnabled(true);
+        jbtn.setToolTipText(hoverText);
+        frameOrPanel.add(jbtn);
+        frameOrPanel.setVisible(true);
+        return jbtn;
+    }
+}
+class showTxtImg{
+    public JLabel putText(Container panelorframe ,String text , int posx , int posy , int sizex , int sizey , String setFont , boolean boldOrNot , int size){
+        JLabel l = new JLabel(text);
+        l.setBounds(posx, posy , sizex, sizey);
+        l.setHorizontalAlignment(JLabel.CENTER);
+        panelorframe.add(l);
+        return l;
+    }
+}
+class textArea{
+    public JTextArea addTextInput(Container panelorframe , int x , int y , int sizex , int sizey){
+        JTextArea textarea = new JTextArea();
+        //textarea.setBounds(x , y , sizex , sizey);
+        textarea.setLineWrap(true);
+        textarea.setWrapStyleWord(true);
+        textarea.setFont(new Font("Arial" , Font.PLAIN , 20));
+        textarea.setForeground(Color.BLACK);
+        textarea.setBackground(Color.WHITE);
+        //panelorframe.add(textarea);
+        JScrollPane scroll = new JScrollPane(textarea);
+        scroll.setBounds(x, y, sizex, sizey);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        panelorframe.add(scroll);
+        return textarea;
+    }
+}
+class TimeCapsule{
+    public static void main(String args[]){
+        createFrame cf = new createFrame();
+        JFrame frame = cf.createFrameFunction("TimeCapsule" , 800 , 600 , 200 , 100);
+        ImageIcon frameBg = new ImageIcon("./assets/frame.png");
+        JLabel frameBgg = new JLabel(frameBg);
+        frame.setContentPane(frameBgg);
+        JPanel panel = cf.createPanelFunction(frame,100,30 , 665 , 510);
+        panel.setBackground(Color.decode("#215B63"));
+        showTxtImg sti = new showTxtImg();
+        JLabel title = sti.putText(frame , "TIMECAPSULE NOTES" , 100 , 0 , 665 , 30 , "Monospaced" , true , 32);
+        title.setForeground(Color.BLACK);
+        title.setBackground(Color.YELLOW);
+        Btn btn = new Btn();
+        JButton importbtn = btn.addbtn(frame,"Import" , "Import Text from filetype" ,0 , 0 ,100 ,50);
+        JButton exportbtn = btn.addbtn(frame,"Export" , "Import Text from filetype" ,0 , 50 ,100 ,50);
+        JButton savebtn = btn.addbtn(frame,"Save" , "Import Text from filetype" ,0 , 100 ,100 ,50);
+        JButton burnbtn = btn.addbtn(frame,"Burn" , "Import Text from filetype" ,0 , 150 ,100 ,50);
+        JButton eleminatebtn = btn.addbtn(frame,"Eleminate" , "Import Text from filetype" ,0 , 200 ,100 ,50);
+        JButton newbtn = btn.addbtn(frame,"New" , "Import Text from filetype" ,0 , 250 ,100 ,50);
+        JButton openbtn = btn.addbtn(frame,"Open" , "Import Text from filetype" ,0 , 300 ,100 ,50);
+        textArea textarea = new textArea();
+        textarea.addTextInput(panel , 10 , 30 , 640 , 460);
+        frame.setVisible(true);
+    }
+}
